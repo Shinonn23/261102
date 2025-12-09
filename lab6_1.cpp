1a551eea7a99cacb65053ed2a9871e758bcbbb3a
Shinonn23 <siwat61719@gmail.com>
2025-12-10 02:53:14 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
+  int x = -1;
+
+  int is_old = 0;
+  int is_even = 0;
+
+  while (x != 0) {
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+    cin >> x;
+    if (x == 0) {
+      break;
+    }
+    if (x % 2 == 0) {
+      is_even += 1;
+    } else {
+      is_old += 1;
+    }
+  }
+
+  cout << "#Even numbers = " << is_even << endl;
+  cout << "#Odd numbers = " << is_old << endl;
+  return 0;
 }
